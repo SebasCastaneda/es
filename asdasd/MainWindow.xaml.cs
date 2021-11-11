09ccc7dffd76e9769c43944bec07bdff66ab6197
SebasCastaneda <carlossebastian.sotocastaneda.stud@ispascalcomandini.it>
2021-11-11 17:58:39 +0100

    codice

--- asdasd/MainWindow.xaml.cs
+++ asdasd/MainWindow.xaml.cs
@@ -23,6 +23,68 @@ namespace asdasd
         public MainWindow()
         {
             InitializeComponent();
+            //codie 
+            //codice
+            //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice //codie v //codie 
+            //codice //codie 
+            //codice
+            //codice //codie  //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice
+            //codice //codie  //codie 
+            //codice //codie 
+            //codice
+            //codice //codie 
+            //codice //codie  //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice
+            //codice //codie 
+            //codice //codie  //c //codie  //codie  // //codie 
+            //codice 
+            //codice
+            //codice //codie 
+            //codiceodie 
+            //codice //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice
+            //codice //codie 
+            //codice //codie 
+            //codie 
+            //codice //codie 
+            //codice //codie 
+            //codie 
+            //codie 
+            //codie 
+            //codie 
+            //codie 
+            //codicev //codie 
+            //codice //codie  //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice
+            //codice //codie 
+            //codice //codie 
+            //codice //codie 
+            //codice
+            //codice
+            //codice
+            //codice
+            //codice
+            //codice
+            //codice
+
+
+
         }
     }
 }
